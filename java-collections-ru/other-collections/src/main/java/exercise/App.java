8565505f0e23e287a305c035dc9e647116d54771
@hexlet/cli <support@hexlet.io>
2023-11-28 14:58:47 +0300

    submit java-collections-ru/other-collections

---
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -0,0 +1,31 @@
+package exercise;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+// BEGIN
+class App {
+    public static Map<String, String> genDiff(Map<String, Object> data1, Map<String, Object> data2) {
+
+        Map<String, String> result = new LinkedHashMap<>();
+        Set<String> keys = new TreeSet<>(data1.keySet());
+        keys.addAll(data2.keySet());
+
+        for (String key : keys) {
+            if (!data1.containsKey(key)) {
+                result.put(key, "added");
+            } else if (!data2.containsKey(key)) {
+                result.put(key, "deleted");
+            } else if (!data1.get(key).equals(data2.get(key))) {
+                result.put(key, "changed");
+            } else {
+                result.put(key, "unchanged");
+            }
+        }
+
+        return result;
+    }
+}
+//END
