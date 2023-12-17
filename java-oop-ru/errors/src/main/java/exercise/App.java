54ccd2ef37148cc756ca1e886baa85351101f8f3
@hexlet/cli <support@hexlet.io>
2023-12-17 23:20:20 +0300

    submit java-oop-ru/errors

--- java-oop-ru/errors/src/main/java/exercise/App.java
+++ java-oop-ru/errors/src/main/java/exercise/App.java
@@ -1,21 +1,18 @@
 package exercise;
 
+import java.text.DecimalFormat;
+
 // BEGIN
 public class App {
 
-    public static void main(String[] args) {
-        Circle circle = new Circle(new Point(3, 3), 4);
-        printSquare(circle);
-    }
-
     public static void printSquare(Circle circle) {
-        String square = null;
         try {
-            square = String.valueOf(Math.round(circle.getSquare()));
+            System.out.println(Math.round(circle.getSquare()));
         } catch (NegativeRadiusException e) {
-            throw new RuntimeException("Не удалось посчитать площадь");
+            System.out.println("Не удалось посчитать площадь");
+        } finally {
+            System.out.println("Вычисление окончено");
         }
-            System.out.println(square + "\nВычисление окончено");
     }
 }
 // END
--- java-oop-ru/errors/src/main/java/exercise/Circle.java
+++ java-oop-ru/errors/src/main/java/exercise/Circle.java
@@ -1,7 +1,22 @@
 package exercise;
 
 // BEGIN
-public record Circle(Point point, int radius) {
+public class Circle {
+    private final Point point;
+    private final int radius;
+
+    public Circle(Point point, int radius) {
+        this.point = point;
+        this.radius = radius;
+    }
+
+    public Point point() {
+        return point;
+    }
+
+    public int radius() {
+        return radius;
+    }
 
     public double getSquare() throws NegativeRadiusException {
         if (radius < 0) {
